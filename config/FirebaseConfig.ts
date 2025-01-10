b27eadd7bdff3b38c486d8757318f4c44029321a
Shreyas Damase <113159691+ShreyasDamase@users.noreply.github.com>
2025-01-10 09:17:06 +0530

    Update FirebaseConfig.ts

--- config/FirebaseConfig.ts
+++ config/FirebaseConfig.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB3TmEljKM0LSvTfhSB01SLg5cIglRmHI",
-  authDomain: "rn-chat-113b0.firebaseapp.com",
-  projectId: "rn-chat-113b0",
-  storageBucket: "rn-chat-113b0.appspot.com",
-  messagingSenderId: "1076131412107",
-  appId: "1:1076131412107:web:d09e4aec32c4071719fc8e",
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
 };
 
 // Initialize Firebase App
